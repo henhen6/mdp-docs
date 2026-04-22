87f96ae7feff6a67d1036250af5b09ffdb3fe433
很很 <tangstruggle@gmail.com>
2026-04-22 16:41:23 +0800

    feat: 重新来

--- docs/.vuepress/components/ColorIcon.ts
+++
@@ -1,8 +0,0 @@
-import { type FunctionalComponent, h } from "vue";
-
-export const ColorIcon: FunctionalComponent<{ icon: string }> = ({ icon }) =>
-  h(
-    "svg",
-    { class: "icon svg-icon", "aria-hidden": "true" },
-    h("use", { "xlink:href": `#icon-${icon}` }),
-  );
--- docs/.vuepress/public/js/baiduGithub.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function () {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?4e89053928e33fece0b18f5edcec4cba";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
